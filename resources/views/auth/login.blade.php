3d04f50e8165c3193a59ea3fe36cba991e35a500
Ferlyn Ruiz <ferlynruiz@elink.corp>
2022-08-03 05:29:28 +0800

    Modify login display added x button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,6 +12,15 @@
             </div>
             <h4 class="text-center">RM Online Tutorials</h4>
             <form action="{{route('authenticate')}}" method="post" class="card p-3 shadow">
+                <div class="w-100 d-flex">
+                    <a href="/" class="ms-auto text-decoration-none text-secondary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                            class="bi bi-x" viewBox="0 0 16 16">
+                            <path
+                                d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                        </svg>
+                    </a>
+                </div>
                 @csrf
                 <div class="form-group my-2">
                     <label for="email">Email</label>
